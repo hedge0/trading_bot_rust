7b4c86111b8f91b4d498ad5d49a3b2b028265502
Elias Martin <elias@sportsvisio.com>
2023-10-28 13:03:42 -0400

    refactored code to use helpers

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,6 +1,9 @@
 use chrono::{Datelike, NaiveDate, Timelike, Utc, Weekday};
 use dotenv::dotenv;
-use std::{env, error::Error, io};
+use ordered_float::OrderedFloat;
+use std::{collections::HashMap, env, error::Error, io};
+
+use crate::structs::{Contender, OrderBody};
 
 // Function that checks if the stock market is currently open.
 pub(crate) fn is_us_stock_market_open(current_time: chrono::DateTime<Utc>) -> bool {
@@ -75,6 +78,157 @@ pub(crate) fn convert_date(input_date: &str) -> String {
     let year_abbreviation: String = parsed_time.format("%y").to_string();
 
     format!("{}{}", month_abbreviation, year_abbreviation)
+}
+
+// Function that builds request data for json body to submit an order.
+pub(crate) fn build_request_data(
+    contender_contracts: &Vec<Contender>,
+    num_fills: i32,
+    account_id: &Option<String>,
+    conids_map: &Option<HashMap<String, HashMap<String, HashMap<OrderedFloat<f64>, String>>>>,
+    discount_value: Option<f64>,
+) -> Vec<OrderBody> {
+    let mut request_data: Vec<OrderBody> = Vec::new();
+
+    for contract in contender_contracts {
+        match contract.type_spread.as_str() {
+            "Calendar" => {
+                request_data.push(build_calendar_order(
+                    contract,
+                    num_fills,
+                    account_id,
+                    conids_map,
+                    discount_value,
+                ));
+            }
+            "Butterfly" => {
+                request_data.push(build_butterfly_order(
+                    contract,
+                    num_fills,
+                    account_id,
+                    conids_map,
+                    discount_value,
+                ));
+            }
+            "Boxspread" => {
+                request_data.push(build_boxspread_order(
+                    contract,
+                    num_fills,
+                    account_id,
+                    conids_map,
+                    discount_value,
+                ));
+            }
+            _ => {}
+        }
+    }
+
+    request_data
+}
+
+// Function that builds calendar order body.
+fn build_calendar_order(
+    contract: &Contender,
+    num_fills: i32,
+    account_id: &Option<String>,
+    conids_map: &Option<HashMap<String, HashMap<String, HashMap<OrderedFloat<f64>, String>>>>,
+    discount_value: Option<f64>,
+) -> OrderBody {
+    OrderBody {
+        acct_id: account_id.clone().unwrap(),
+        con_idex: format!(
+            "28812380;;;{}/-1,{}",
+            conids_map.as_ref().unwrap()[contract.contracts[0].date.as_str()]
+                [contract.contracts[0].type_contract.as_str()]
+                [(&contract.contracts[0].strike).into()],
+            conids_map.as_ref().unwrap()[contract.contracts[1].date.as_str()]
+                [contract.contracts[1].type_contract.as_str()]
+                [(&contract.contracts[1].strike).into()]
+        ),
+        order_type: "LMT".to_string(),
+        listing_exchange: "SMART".to_string(),
+        outside_rth: false,
+        price: -1.0 * (contract.arb_val * discount_value.unwrap()).round(),
+        side: "BUY".to_string(),
+        ticker: "SPX".to_string(),
+        tif: "DAY".to_string(),
+        referrer: "NO_REFERRER_PROVIDED".to_string(),
+        quantity: num_fills,
+        use_adaptive: false,
+    }
+}
+
+// Function that builds calendar order body.
+fn build_butterfly_order(
+    contract: &Contender,
+    num_fills: i32,
+    account_id: &Option<String>,
+    conids_map: &Option<HashMap<String, HashMap<String, HashMap<OrderedFloat<f64>, String>>>>,
+    discount_value: Option<f64>,
+) -> OrderBody {
+    OrderBody {
+        acct_id: account_id.clone().unwrap(),
+        con_idex: format!(
+            "28812380;;;{}/-2,{},{}",
+            conids_map.as_ref().unwrap()[contract.contracts[1].date.as_str()]
+                [contract.contracts[1].type_contract.as_str()]
+                [(&contract.contracts[1].strike).into()],
+            conids_map.as_ref().unwrap()[contract.contracts[0].date.as_str()]
+                [contract.contracts[0].type_contract.as_str()]
+                [(&contract.contracts[0].strike).into()],
+            conids_map.as_ref().unwrap()[contract.contracts[2].date.as_str()]
+                [contract.contracts[2].type_contract.as_str()]
+                [(&contract.contracts[2].strike).into()]
+        ),
+        order_type: "LMT".to_string(),
+        listing_exchange: "SMART".to_string(),
+        outside_rth: false,
+        price: -1.0 * (contract.arb_val * discount_value.unwrap()).round(),
+        side: "BUY".to_string(),
+        ticker: "SPX".to_string(),
+        tif: "DAY".to_string(),
+        referrer: "NO_REFERRER_PROVIDED".to_string(),
+        quantity: num_fills,
+        use_adaptive: false,
+    }
+}
+
+// Function that builds calendar order body.
+fn build_boxspread_order(
+    contract: &Contender,
+    num_fills: i32,
+    account_id: &Option<String>,
+    conids_map: &Option<HashMap<String, HashMap<String, HashMap<OrderedFloat<f64>, String>>>>,
+    discount_value: Option<f64>,
+) -> OrderBody {
+    OrderBody {
+        acct_id: account_id.clone().unwrap(),
+        con_idex: format!(
+            "28812380;;;{}/-1,{},{},{}",
+            conids_map.as_ref().unwrap()[contract.contracts[3].date.as_str()]
+                [contract.contracts[3].type_contract.as_str()]
+                [(&contract.contracts[3].strike).into()],
+            conids_map.as_ref().unwrap()[contract.contracts[2].date.as_str()]
+                [contract.contracts[2].type_contract.as_str()]
+                [(&contract.contracts[2].strike).into()],
+            conids_map.as_ref().unwrap()[contract.contracts[0].date.as_str()]
+                [contract.contracts[0].type_contract.as_str()]
+                [(&contract.contracts[0].strike).into()],
+            conids_map.as_ref().unwrap()[contract.contracts[1].date.as_str()]
+                [contract.contracts[1].type_contract.as_str()]
+                [(&contract.contracts[1].strike).into()]
+        ),
+        order_type: "LMT".to_string(),
+        listing_exchange: "SMART".to_string(),
+        outside_rth: false,
+        price: -1.0 * ((contract.arb_val * discount_value.unwrap()) + 5.0).round(),
+        side: "BUY".to_string(),
+        ticker: "SPX".to_string(),
+        tif: "DAY".to_string(),
+        referrer: "NO_REFERRER_PROVIDED".to_string(),
+        quantity: num_fills,
+        use_adaptive: false,
+    }
 }
 
 // Function that gets input and returns result.
