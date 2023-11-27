80a0c1e529e5fbe6d090978ba434b0efafc3a0fd
hedge0 <eliasmemail@gmail.com>
2023-11-26 19:40:44 -0500

    fixed functions and main

--- src/main.rs
+++ src/main.rs
@@ -67,44 +67,44 @@ fn main() {
                 let start_time: Instant = Instant::now();
                 let mut end_time: Option<Duration> = None;
 
-                //            match active_tick.get_contender_contracts(&option, num_orders) {
-                //                Ok(contender_contracts) => {
-                //                    if !contender_contracts.is_empty() {
-                //                        if mode {
-                //                           match ibkr
-                //                                .order_contender_contracts(&contender_contracts, num_fills)
-                //                            {
-                //                                Ok(_) => log_message(format!("Ordering Contracts...")),
-                //                                Err(e) => log_error(format!("{}", e)),
-                //                            }
-                //                        }
-                //                        end_time = Some(start_time.elapsed());
-                //                        for contender in contender_contracts {
-                //                            log_message(format!(
-                //                                "Submitting Order for {} * {} {} @ {:.2}:",
-                //                                num_fills,
-                //                                contender.type_spread,
-                //                                contender.exp_date,
-                //                                contender.arb_val
-                //                            ));
+                match ibkr.get_contender_contracts(&option, num_orders) {
+                    Ok(contender_contracts) => {
+                        if !contender_contracts.is_empty() {
+                            if mode {
+                                match ibkr
+                                    .order_contender_contracts(&contender_contracts, num_fills)
+                                {
+                                    Ok(_) => log_message(format!("Ordering Contracts...")),
+                                    Err(e) => log_error(format!("{}", e)),
+                                }
+                            }
+                            end_time = Some(start_time.elapsed());
+                            for contender in contender_contracts {
+                                log_message(format!(
+                                    "Submitting Order for {} * {} {} @ {:.2}:",
+                                    num_fills,
+                                    contender.type_spread,
+                                    contender.exp_date,
+                                    contender.arb_val
+                                ));
 
-                //                            for i in 0..contender.contracts.len() {
-                //                               log_message(format!(
-                //                                  "\tLeg {}: {} {} * {:.2}{} {} @ {:.2}",
-                //                                    i + 1,
-                //                                    contender.action(i),
-                //                                    contender.multiplier(num_fills, i),
-                //                                    contender.contracts[i].strike as i64,
-                //                                    contender.contracts[i].type_contract,
-                //                                    contender.contracts[i].date,
-                //                                    contender.contracts[i].mkt_price
-                //                                ));
-                //                            }
-                //                        }
-                //                    }
-                //                }
-                //                Err(e) => log_error(format!("{}", e)),
-                //            }
+                                for i in 0..contender.contracts.len() {
+                                    log_message(format!(
+                                        "\tLeg {}: {} {} * {:.2}{} {} @ {:.2}",
+                                        i + 1,
+                                        contender.action(i),
+                                        contender.multiplier(num_fills, i),
+                                        contender.contracts[i].strike as i64,
+                                        contender.contracts[i].type_contract,
+                                        contender.contracts[i].date,
+                                        contender.contracts[i].mkt_price
+                                    ));
+                                }
+                            }
+                        }
+                    }
+                    Err(e) => log_error(format!("{}", e)),
+                }
 
                 // Record the current time after running the program.
                 if let Some(duration) = end_time {
