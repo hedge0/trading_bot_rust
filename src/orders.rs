2036d6b6e10811ca5ad10a7210305245fef1c9f2
hedge0 <eliasmemail@gmail.com>
2023-11-29 01:02:40 -0500

    added orders

--- src/orders.rs
+++ src/orders.rs
@@ -4,32 +4,30 @@ use ordered_float::OrderedFloat;
 
 use crate::structs::{Contender, OrderBody, RequestDataStruct};
 
-// Function that builds butterfly order body.
-pub(crate) fn build_butterfly_order(
+// Function that builds butterfly bull order body.
+pub(crate) fn build_butterfly_bull_order(
     contract: &Contender,
     num_fills: i32,
     account_id: &Option<String>,
     conids_map: &Option<HashMap<String, HashMap<String, HashMap<OrderedFloat<f64>, String>>>>,
     discount_value: Option<f64>,
 ) -> OrderBody {
+    let order_val: f64 = contract.contracts[0].mkt_price - contract.contracts[1].mkt_price;
     OrderBody {
         acct_id: account_id.clone().unwrap(),
         con_idex: format!(
-            "28812380;;;{}/-2,{}/1,{}/1",
+            "28812380;;;{}/-1,{}/1",
             conids_map.as_ref().unwrap()[contract.contracts[1].date.as_str()]
                 [contract.contracts[1].type_contract.as_str()]
                 [(&contract.contracts[1].strike).into()],
             conids_map.as_ref().unwrap()[contract.contracts[0].date.as_str()]
                 [contract.contracts[0].type_contract.as_str()]
-                [(&contract.contracts[0].strike).into()],
-            conids_map.as_ref().unwrap()[contract.contracts[2].date.as_str()]
-                [contract.contracts[2].type_contract.as_str()]
-                [(&contract.contracts[2].strike).into()]
+                [(&contract.contracts[0].strike).into()]
         ),
         order_type: "LMT".to_string(),
         listing_exchange: "SMART".to_string(),
         outside_rth: false,
-        price: -1.0 * (((contract.arb_val - discount_value.unwrap()) * 100.0).round() / 100.0),
+        price: -1.0 * (((order_val - discount_value.unwrap()) * 100.0).round() / 100.0),
         side: "BUY".to_string(),
         ticker: "SPX".to_string(),
         tif: "DAY".to_string(),
@@ -39,27 +37,88 @@ pub(crate) fn build_butterfly_order(
     }
 }
 
-// Function that builds boxspread order body.
-pub(crate) fn build_boxspread_order(
+// Function that builds butterfly bear order body.
+pub(crate) fn build_butterfly_bear_order(
     contract: &Contender,
     num_fills: i32,
     account_id: &Option<String>,
     conids_map: &Option<HashMap<String, HashMap<String, HashMap<OrderedFloat<f64>, String>>>>,
     discount_value: Option<f64>,
 ) -> OrderBody {
+    let order_val: f64 = contract.contracts[2].mkt_price - contract.contracts[1].mkt_price;
     OrderBody {
         acct_id: account_id.clone().unwrap(),
         con_idex: format!(
-            "28812380;;;{}/-1,{}/1,{}/1,{}/-1",
+            "28812380;;;{}/-1,{}/1",
+            conids_map.as_ref().unwrap()[contract.contracts[1].date.as_str()]
+                [contract.contracts[1].type_contract.as_str()]
+                [(&contract.contracts[1].strike).into()],
+            conids_map.as_ref().unwrap()[contract.contracts[2].date.as_str()]
+                [contract.contracts[2].type_contract.as_str()]
+                [(&contract.contracts[2].strike).into()]
+        ),
+        order_type: "LMT".to_string(),
+        listing_exchange: "SMART".to_string(),
+        outside_rth: false,
+        price: -1.0 * (((order_val - discount_value.unwrap()) * 100.0).round() / 100.0),
+        side: "BUY".to_string(),
+        ticker: "SPX".to_string(),
+        tif: "DAY".to_string(),
+        referrer: "NO_REFERRER_PROVIDED".to_string(),
+        quantity: num_fills,
+        use_adaptive: false,
+    }
+}
+
+// Function that builds boxspread put order body.
+pub(crate) fn build_boxspread_put_order(
+    contract: &Contender,
+    num_fills: i32,
+    account_id: &Option<String>,
+    conids_map: &Option<HashMap<String, HashMap<String, HashMap<OrderedFloat<f64>, String>>>>,
+    discount_value: Option<f64>,
+) -> OrderBody {
+    let order_val: f64 = contract.contracts[0].mkt_price - contract.contracts[3].mkt_price;
+    OrderBody {
+        acct_id: account_id.clone().unwrap(),
+        con_idex: format!(
+            "28812380;;;{}/-1,{}/1",
             conids_map.as_ref().unwrap()[contract.contracts[3].date.as_str()] // Short right dated Put.
                 [contract.contracts[3].type_contract.as_str()]
                 [(&contract.contracts[3].strike).into()],
+            conids_map.as_ref().unwrap()[contract.contracts[0].date.as_str()] // Long current dated Put.
+                [contract.contracts[0].type_contract.as_str()]
+                [(&contract.contracts[0].strike).into()]
+        ),
+        order_type: "LMT".to_string(),
+        listing_exchange: "SMART".to_string(),
+        outside_rth: false,
+        price: -1.0 * (((order_val - discount_value.unwrap()) * 100.0).round() / 100.0),
+        side: "BUY".to_string(),
+        ticker: "SPX".to_string(),
+        tif: "DAY".to_string(),
+        referrer: "NO_REFERRER_PROVIDED".to_string(),
+        quantity: num_fills,
+        use_adaptive: false,
+    }
+}
+
+// Function that builds boxspread call order body.
+pub(crate) fn build_boxspread_call_order(
+    contract: &Contender,
+    num_fills: i32,
+    account_id: &Option<String>,
+    conids_map: &Option<HashMap<String, HashMap<String, HashMap<OrderedFloat<f64>, String>>>>,
+    discount_value: Option<f64>,
+) -> OrderBody {
+    let order_val: f64 = contract.contracts[2].mkt_price - contract.contracts[1].mkt_price;
+    OrderBody {
+        acct_id: account_id.clone().unwrap(),
+        con_idex: format!(
+            "28812380;;;{}/1,{}/-1",
             conids_map.as_ref().unwrap()[contract.contracts[2].date.as_str()] // Long right dated Call.
                 [contract.contracts[2].type_contract.as_str()]
                 [(&contract.contracts[2].strike).into()],
-            conids_map.as_ref().unwrap()[contract.contracts[0].date.as_str()] // Long current dated Put.
-                [contract.contracts[0].type_contract.as_str()]
-                [(&contract.contracts[0].strike).into()],
             conids_map.as_ref().unwrap()[contract.contracts[1].date.as_str()] // Short current dated Call.
                 [contract.contracts[1].type_contract.as_str()]
                 [(&contract.contracts[1].strike).into()]
@@ -67,7 +126,7 @@ pub(crate) fn build_boxspread_order(
         order_type: "LMT".to_string(),
         listing_exchange: "SMART".to_string(),
         outside_rth: false,
-        price: -1.0 * (((contract.arb_val - discount_value.unwrap()) * 100.0).round() / 100.0),
+        price: -1.0 * (((order_val - discount_value.unwrap()) * 100.0).round() / 100.0),
         side: "BUY".to_string(),
         ticker: "SPX".to_string(),
         tif: "DAY".to_string(),
@@ -90,14 +149,14 @@ pub(crate) fn build_request_data(
     for contract in contender_contracts {
         match contract.type_spread.as_str() {
             "Butterfly" => {
-                request_data.orders.push(build_butterfly_order(
+                request_data.orders.push(build_butterfly_bull_order(
                     contract,
                     num_fills,
                     account_id,
                     conids_map,
                     discount_value,
                 ));
-                request_data.orders.push(build_butterfly_order(
+                request_data.orders.push(build_butterfly_bear_order(
                     contract,
                     num_fills,
                     account_id,
@@ -106,14 +165,14 @@ pub(crate) fn build_request_data(
                 ));
             }
             "Boxspread" => {
-                request_data.orders.push(build_boxspread_order(
+                request_data.orders.push(build_boxspread_put_order(
                     contract,
                     num_fills,
                     account_id,
                     conids_map,
                     discount_value,
                 ));
-                request_data.orders.push(build_boxspread_order(
+                request_data.orders.push(build_boxspread_call_order(
                     contract,
                     num_fills,
                     account_id,
